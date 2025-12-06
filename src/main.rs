a47755c780f086c28cca65ef45d9af08eb0031bf
Aniketh Dandu <aniketh.dandu@icloud.com>
2025-12-06 04:29:20 -0500

    Add implementation for F opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use std::{fs};
+use std::{fs, process};
 
 extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point};
-use std::time::{Instant, Duration};
+use sdl2::rect::Point;
+use std::time::{Duration, Instant};
 
 // Define number of instructions per frame
 const IPF: i32 = 500;
 
 // Define path to ROM
-const PROGRAM_PATH: &str = "programs/ibm.ch8";
+const PROGRAM_PATH: &str = "programs/randnum2.ch8";
 
-fn u8_to_bits(num:u8) -> [bool; 8] {
-    let mut bitarray: [bool;8] = [false; 8];
+// TODO: Add unit test for u8 helper functions
+// TODO: Finish implementing opcodes
+// TODO: Test on more programs
+// TODO: Split opcode and draw loop onto separate threads (?)
+
+fn u8_to_bits(num: u8) -> [bool; 8] {
+    let mut bitarray: [bool; 8] = [false; 8];
     for i in 0..8 {
-        bitarray[7-i] = ((num >> i) & 1) == 1;
+        bitarray[7 - i] = ((num >> i) & 1) == 1;
     }
     return bitarray;
 }
@@ -33,20 +38,19 @@ fn bits_to_num(num: &[bool]) -> u32 {
     return ret_val;
 }
 
-fn u8_to_hex(number:u8) -> String {
+fn u8_to_hex(number: u8) -> String {
     let num: [bool; 8] = u8_to_bits(number);
     let num_len = num.len();
     let num_nibbles = num_len / 4;
     let remainder = num_len % 4;
     let mut ret_str = String::with_capacity(num_len);
     for i in 0..num_nibbles {
-        let hex_str = bits_to_num(&num[(4*i)..(4*(i+1))]);
+        let hex_str = bits_to_num(&num[(4 * i)..(4 * (i + 1))]);
         ret_str.push_str(format!("{:X}", hex_str).as_str());
     }
     if remainder != 0 {
-        let remain_num = bits_to_num(&num[4*num_nibbles..]);
+        let remain_num = bits_to_num(&num[4 * num_nibbles..]);
         ret_str.push_str(format!("{:X}", remain_num).as_str());
-
     }
     return ret_str;
 }
@@ -59,6 +63,7 @@ pub fn main() -> Result<(), String> {
     // Initialize registers, pointers, and memory
     let mut pc: usize = 0x200;
     let mut index: usize = 0;
+
     // NOTE: register F is flag register (can be set to 0 or 1)
     let mut registers: [u8; 16] = [0; 16];
     let mut stack: Vec<usize> = vec![];
@@ -130,72 +135,74 @@ pub fn main() -> Result<(), String> {
         if !throttle {
             instruction_count += 1;
 
-            let opcode: String = format!("{}{}",u8_to_hex(memory[pc]),u8_to_hex(memory[pc+1]));
+            let opcode: String = format!("{}{}", u8_to_hex(memory[pc]), u8_to_hex(memory[pc + 1]));
             // println!("{}", opcode);
             // let start_nib: char = opcode.chars().next().unwrap();
             let nibbles_char: Vec<char> = opcode.chars().collect();
-            let nibbles_usize: Vec<usize> = opcode.chars().map(|c| c.to_digit(16).unwrap() as usize).collect();
+            let nibbles_usize: Vec<usize> = opcode
+                .chars()
+                .map(|c| c.to_digit(16).unwrap() as usize)
+                .collect();
             let nibble_last_three = usize::from_str_radix(&opcode[1..], 16).unwrap();
             let nibble_last_two = u8::from_str_radix(&opcode[2..], 16).unwrap();
 
             pc += 2;
 
             match nibbles_char.first().expect("No opcode found!") {
-                '0' => {
-                    match opcode.as_str() {
-                        "00E0" => {
-                            canvas.set_draw_color(Color::RGB(0, 0, 0));
-                            canvas.clear();
-                            disp_mem = [false; 2048];
-                        },
-                        "00EE" => {
-                            pc = stack.pop().unwrap();
-                        },
-                        _ => {
-                            println!("Instruction not found!");
-                            println!("{}",opcode);
-                        }
+                '0' => match opcode.as_str() {
+                    "00E0" => {
+                        canvas.set_draw_color(Color::RGB(0, 0, 0));
+                        canvas.clear();
+                        disp_mem = [false; 2048];
+                    }
+                    "00EE" => {
+                        pc = stack.pop().unwrap();
+                    }
+                    _ => {
+                        println!("Instruction not found!");
+                        println!("{}", opcode);
                     }
                 },
                 '1' => {
                     // 1NNN
                     // Jump to addr (set program counter)
-                    pc  = nibble_last_three;
-                },
+                    pc = nibble_last_three;
+                }
                 '2' => {
                     // 2NNN
                     stack.push(pc);
                     pc = nibble_last_three;
-                },
+                }
                 '3' => {
                     // 3XNN
                     if registers[nibbles_usize[1]] == nibble_last_two {
                         pc += 2;
                     }
-                },
+                }
                 '4' => {
                     // 4XNN
                     if registers[nibbles_usize[1]] != nibble_last_two {
                         pc += 2;
                     }
-                },
+                }
                 '5' => {
                     // 5XY0
                     if registers[nibbles_usize[1]] == registers[nibbles_usize[1]] {
                         pc += 2;
                     }
-                },
+                }
                 '6' => {
                     // 6XNN
                     // Set register X to NN
                     registers[nibbles_usize[1]] = nibble_last_two;
-                },
+                }
                 '7' => {
                     // 7XNN
                     // Add NN to register X
                     // registers[nibbles_usize[1]] += nibble_last_two;
-                    registers[nibbles_usize[1]] = add_u8_with_overflow(&registers[nibbles_usize[1]],&nibble_last_two);
-                },
+                    registers[nibbles_usize[1]] =
+                        add_u8_with_overflow(&registers[nibbles_usize[1]], &nibble_last_two);
+                }
                 '8' => {
                     // match end_char {
                     //     '0' => {
@@ -205,15 +212,18 @@ pub fn main() -> Result<(), String> {
 
                     //     },
                     // }
-                },
+                }
                 'A' => {
                     // ANNN
                     // Set index to NNN
-                    index = nibble_last_three; 
-                },
+                    index = nibble_last_three;
+                }
                 'B' => {
                     pc = nibble_last_three + registers[0] as usize;
-                },
+                }
+                'C' => {
+                    registers[nibbles_usize[1]] = rand::random::<u8>() & nibble_last_two;
+                }
                 // TODO: Fix timing to remove jittering
                 'D' => {
                     // DXYN
@@ -237,20 +247,51 @@ pub fn main() -> Result<(), String> {
                                     disp_mem[disp_offset] = true;
                                     canvas.set_draw_color(Color::RGB(255, 255, 255));
                                 }
-                                canvas.draw_point(Point::new(col as i32, row as i32)).unwrap();
+                                canvas
+                                    .draw_point(Point::new(col as i32, row as i32))
+                                    .unwrap();
                             }
                         }
                     }
-                },
-                'F' => {
-                    if &opcode[2..] == "1E" {
-                        index += registers[opcode.chars().nth(1).unwrap().to_digit(16).unwrap() as usize] as usize;
+                }
+                'F' => match &opcode[2..] {
+                    "07" => {
+                        // Timer
                     }
+                    "0A" => {
+                        // Key Press
+                    }
+                    "15" => {
+                        // Delay Timer
+                    }
+                    "18" => {
+                        // Sound Timer
+                    }
+                    "1E" => {
+                        index += registers[nibbles_usize[1]] as usize;
+                    }
+                    "29" => {
+                        index = memory[0x50 + 5 * registers[nibbles_usize[1]] as usize] as usize;
+                    }
+                    "33" => {
+                        let num_str = format!("{:0>3}", registers[nibbles_usize[1]]);
+                        for i in 0..3 {
+                            memory[index + i] = num_str.chars().nth(i).unwrap() as u8;
+                        }
+                    }
+                    "65" => {
+                        for i in 0..16 {
+                            registers[i] = memory[index + i];
+                        }
+                    }
+
+                    _ => {}
                 },
                 _ => {
-                    // println!("{}", opcode);
-                    break;
-                },
+                    println!("{}", opcode);
+                    process::exit(1);
+                    // break;
+                }
             }
         }
 
@@ -259,7 +300,7 @@ pub fn main() -> Result<(), String> {
             instruction_count = 0;
         }
 
-        if start_time.elapsed() > Duration::from_millis(16) {
+        if start_time.elapsed() > Duration::from_millis(8) {
             throttle = false;
             start_time = Instant::now();
             canvas.present();
